7340d2059a28f6efed48723eb3f8ab3b1352efa7
Lori M Olson <lori@wndx.com>
2013-02-12 11:21:34 -0700

    Lori's customizations

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails3BootstrapDeviseCancan::Application.config.secret_token = 'a6cd68af2c0c93e2f0bd4c576aab509f59b7ae839d02018caa4c8ce3cc46ea012fdc4f1dc93bbb14137bc646fb697907e9649260d02b77eed838717d20dff479'
+Rails3BootstrapDeviseCancan::Application.config.secret_token = '9aa18d6d8a33a3bdbdb1260dba35b7ea7e71d20e4106ca2ce38cf194bf5d6f0e584615cd2eda38ebc59529cd2031b12d81b9cf002bb43702997d480d81e2c94a'
